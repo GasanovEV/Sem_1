a8f9e4dc0d8223eeef585fc4d6892876340bd386
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-15 00:28:48 +0300

    Add Example002_ExponentationCheck

---
+++ Example002_ExponentationCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write ("Введите первое число ");
+string userInput1 = Console.ReadLine () ??"";
+int number1 = int.Parse (userInput1);
+Console.Write ("Введите второе число ");
+string userInput2 = Console.ReadLine () ??"";
+int number2 = int.Parse (userInput2);
+
+if (number1 == number2*number2)
+    {
+        Console.WriteLine ($"{number1},{number2} -> да");
+    }
+else 
+{
+    Console.WriteLine ($"{number1},{number2} -> нет");
+}
+
