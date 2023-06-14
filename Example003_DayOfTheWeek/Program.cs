7953b02b2d51ba05f161dd37c3f9314a709b3ed4
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-15 00:55:34 +0300

    Add Example003_DayOfTheWeek

---
+++ Example003_DayOfTheWeek/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.Clear();
+Console.Write("День недели ");
+string userInput1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+
+if (number1 == 1)
+{
+    Console.WriteLine($"{number1} -> Понедельник");
+}
+else
+{
+    if (number1 == 2)
+    {
+        Console.WriteLine($"{number1} -> Вторник");
+    }
+    else
+    {
+        if (number1 == 3)
+            {
+                Console.WriteLine($"{number1} -> Среда");
+            }
+    
+         else 
+            {
+                if (number1 == 4)
+                    {
+                     Console.WriteLine($"{number1} -> Четверг");
+                    }
+                else
+                    {
+                        if (number1 == 5)
+                            {
+                                Console.WriteLine($"{number1} -> Пятница");
+                            }
+                        else
+                            {
+                                if (number1 == 6)
+                                    {
+                                        Console.WriteLine($"{number1} -> Суббота");
+                                    }
+                                else
+                                        {
+                                            if (number1 == 7)
+                                                {
+                                                        Console.WriteLine($"{number1} -> Воскресение");
+                                                }
+                                                    else
+    
+                                                        {
+                                                            Console.WriteLine("Такого дня недели нет");
+                                                         }
+}
+    
+    }
+                    }
+            }
+    }
+}
